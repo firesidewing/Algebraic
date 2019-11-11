c8c14ff477459a15688bb548c218dd140ad4767e
Josh <dollingerjosh@gmail.com>
2019-11-10 22:18:53 -0500

    working model

--- README.md
+++ README.md
@@ -20,5 +20,10 @@ npm run build
 npm run lint
 ```
 
+### Electron Build
+```
+npm run electron:build
+```
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Algebraic</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field label="This" v-model="Relative1" single-line solo clearable></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field label="Relative to this" v-model="Relative2" single-line solo clearable></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title class="display-2">Percentage</v-card-title>
+              <v-card-text class="display-4">{{ result }}</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title class="display-2">Difference</v-card-title>
+              <v-card-text class="display-4">{{ diff }}</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  props: {
+    source: String
+  },
+  data: () => ({
+    Relative1: null,
+    Relative2: null
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+  computed: {
+    result: function() {
+      if (this.Relative1 != null && this.Relative2 != null) {
+        if (this.Relative1 / this.Relative2 > 1) {
+          return (
+            ((this.Relative1 / this.Relative2) * 100 - 100).toFixed(2) +
+            "% Increase"
+          );
+        } else {
+          return Math.abs((this.Relative1 / this.Relative2) * 100 - 100).toFixed(2) + "% Decrease";
+        }
+      } else {
+        return "";
+      }
+    },
+    diff: function() {
+      if (this.Relative1 != null && this.Relative2 != null) {
+        return Math.abs(this.Relative1 - this.Relative2);
+      } else {
+        return "";
+      }
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
