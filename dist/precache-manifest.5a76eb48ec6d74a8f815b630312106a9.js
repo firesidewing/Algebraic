97b1597434065364c31423656cf4238785975684
Josh <josh.dollinger@raptorint.ca>
2020-02-22 16:09:29 -0500

    pwa

---
+++ dist/precache-manifest.5a76eb48ec6d74a8f815b630312106a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dd6a719e17bb5c88a1",
+    "url": "/css/app.afd22836.css"
+  },
+  {
+    "revision": "1891001a32e01ad76541",
+    "url": "/css/chunk-vendors.b6e1724a.css"
+  },
+  {
+    "revision": "79c358cad1424decf48fec23f759ff20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1dd6a719e17bb5c88a1",
+    "url": "/js/app.f3e9b610.js"
+  },
+  {
+    "revision": "1891001a32e01ad76541",
+    "url": "/js/chunk-vendors.713d8201.js"
+  },
+  {
+    "revision": "038192f82623324531d05159c3e99c73",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
